dd63840c66106b344974185d59e67f8e3e000ce0
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-11-29 01:20:48 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-import Grid from "./Grid.js";
 
 window.onload = function() {
-  <Grid />;
   console.log("Hello Rigo from the console!");
 };
